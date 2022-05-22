8d72267dbdaf70b86c6a6ddfd1320a1259472565
Sergio Valadez <sergioval@gmail.com>
2022-05-22 13:47:47 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+function App() {
+  return <h1>Rick and Morty</h1>;
+}
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
